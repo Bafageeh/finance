ceedc2dad1ea3b273a63e6b093c5bc624a09de28
Bafageeh <a.baf@live.com>
2026-05-01 14:51:07 +0300

    Scope core finance models by account

--- finance-api/app/Providers/AppServiceProvider.php
+++ finance-api/app/Providers/AppServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\Client;
+use App\Models\ClientReminder;
+use App\Models\FollowUp;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +23,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        foreach ([Client::class, FollowUp::class, ClientReminder::class] as $modelClass) {
+            $modelClass::addGlobalScope('account', function (Builder $builder) {
+                $accountId = request()?->user()?->account_id;
+
+                if ($accountId) {
+                    $builder->where($builder->getModel()->getTable() . '.account_id', $accountId);
+                }
+            });
+
+            $modelClass::creating(function ($model) {
+                if (! $model->account_id) {
+                    $model->account_id = request()?->user()?->account_id;
+                }
+            });
+        }
     }
 }
